7a5ec9240a45175780047394b79cd97bad960018
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-05-31 00:48:43 +0100

    FIX: Comply with coding standards

--- src/RandomEnglishGenerator.php
+++ src/RandomEnglishGenerator.php
@@ -1,12 +1,11 @@
-<?php
+<?php declare(strict_types = 1);
+
 namespace Suilven\RandomEnglish;
 
 class RandomEnglishGenerator
 {
-    /** @var string configuration file for sentences */
-    private $config = '';
 
-    const POSSIBLE_WORD_TYPES = [
+    private const POSSIBLE_WORD_TYPES = [
         'noun',
         'countable_noun',
 
@@ -32,21 +31,20 @@ class RandomEnglishGenerator
 
     ];
 
+    /** @var string configuration file for sentences */
+    private string $config = '';
+
     public function __construct()
     {
-        $configFile = dirname(__FILE__) . '/../config/sentence-structure.cfg';
-        $this->setConfig(file_get_contents($configFile));
+        $configFile = \dirname(__FILE__) . '/../config/sentence-structure.cfg';
+        $this->setConfig(\file_get_contents($configFile));
     }
 
-
-    /**
-     * @param string $newConfig configuration file of how sentence structures
-     */
-    public function setConfig($newConfig) : void
+    /** @param string $newConfig configuration file of how sentence structures */
+    public function setConfig(string $newConfig): void
     {
-        $this->config = trim($newConfig);
+        $this->config = \trim($newConfig);
     }
-
 
     /**
      * Generate a random sentence
@@ -54,113 +52,115 @@ class RandomEnglishGenerator
      * @param bool $titleCase true to generate a title, false (default) to generate a sentence
      * @return string a random sentence
      */
-    public function sentence($titleCase = false)
+    public function sentence(bool $titleCase = false): string
     {
-        $structures = explode(PHP_EOL, $this->config);
-        shuffle($structures);
+        $structures = \explode(\PHP_EOL, $this->config);
+        \shuffle($structures);
         $structure = $structures[0];
         
         $expression='/[\s]+/';
-        $splits = preg_split($expression, $structure, -1, PREG_SPLIT_NO_EMPTY);
+        $splits = \preg_split($expression, $structure, -1, \PREG_SPLIT_NO_EMPTY);
 
         $sentenceArray = [];
+
         foreach ($splits as $possiblyRandomWord) {
             $randomized = false;
+
             foreach (self::POSSIBLE_WORD_TYPES as $wordType) {
                 $start = '[' . $wordType . ']';
-                if (substr($possiblyRandomWord, 0, strlen($start)) === $start) {
-                    $restOfWord = str_replace($start, '', $possiblyRandomWord);
+
+                if (\substr($possiblyRandomWord, 0, \strlen($start)) === $start) {
+                    $restOfWord = \str_replace($start, '', $possiblyRandomWord);
                     $sentenceArray[] = $this->getRandomWord($wordType) . $restOfWord;
                     $randomized = true;
+
                     break;
                 }
             }
 
-            if (!$randomized) {
-                $sentenceArray[] = $possiblyRandomWord;
+            if ($randomized) {
+                continue;
             }
+
+            $sentenceArray[] = $possiblyRandomWord;
         }
 
         // ensure sentence starts with a capital
-        $sentenceArray[0] = ucfirst($sentenceArray[0]);
+        $sentenceArray[0] = \ucfirst($sentenceArray[0]);
 
-        $result = implode(" ", $sentenceArray) . '.';
+        $result = \implode(" ", $sentenceArray) . '.';
+
         if ($titleCase) {
-            $result = ucwords($result);
-            $result = substr_replace($result, "", -1);
+            $result = \ucwords($result);
+            $result = \substr_replace($result, "", -1);
         }
 
         return $result;
     }
 
-
     /**
      * Generate a random title
+     *
      * @return string a random sentence, all in caps, no trailing full stop
      */
-    public function title()
+    public function title(): string
     {
         return $this->sentence(true);
     }
-
 
     /**
      * @param int $maxSentences The maximum number of sentences
      * @return string a paragraph of random sentences
      */
-    public function paragraph($maxSentences = 10)
+    public function paragraph(int $maxSentences = 10): string
     {
-        $nParagraph = rand(1, $maxSentences);
+        $nParagraph = \rand(1, $maxSentences);
         $sentences = [];
-        for ($i=0; $i< $maxSentences; $i++) {
+
+        for ($i=0; $i< $nParagraph; $i++) {
             $sentences[] = $this->sentence();
         }
 
-        return implode("  ", $sentences);
+        return \implode("  ", $sentences);
     }
 
-
-    /**
-     * @return string a plural noun
-     */
-    public function pluralNoun()
+    /** @return string a plural noun */
+    public function pluralNoun(): string
     {
         $plurableNoun = $this->getRandomWord('countable_noun');
+
         return $plurableNoun . 's';
     }
 
-
-    /**
-     * @return string a plural verb
-     */
-    public function pluralVerb()
+    /** @return string a plural verb */
+    public function pluralVerb(): string
     {
         // @todo Choose a random verb source file
         $plurableNoun = $this->getRandomWord('verb');
+
         return $plurableNoun . 's';
     }
 
-
-    /**
-     * @return string doing version of a verb
-     */
-    public function verbing()
+    /** @return string doing version of a verb */
+    public function verbing(): string
     {
         // @todo Choose a random verb source file
         $plurableNoun = $this->getRandomWord('verb');
+
         return $plurableNoun . 'ing';
     }
-
 
     /**
      * @param string $wordType the type of word, e.g. noun, verb
      * @return string a random word for the given word type
      */
-    private function getRandomWord($wordType)
+    private function getRandomWord(string $wordType): string
     {
-        $wordsFile = dirname(__FILE__) . '/../words/english_' . $wordType . 's.txt';
-        $words = explode(PHP_EOL, trim(file_get_contents($wordsFile)));
-        shuffle($words);
+        $wordsFile = \dirname(__FILE__) . '/../words/english_' . $wordType . 's.txt';
+        $words = \explode(\PHP_EOL, \trim(\file_get_contents($wordsFile)));
+        \shuffle($words);
+
         return $words[0];
     }
+
 }
--- tests/RandomEnglishGeneratorTest.php
+++ tests/RandomEnglishGeneratorTest.php
@@ -17,42 +17,41 @@ class RandomEnglishGeneratorTest extends TestCase
 
     public function testSentence(): void
     {
-        $generator = new RandomEnglishGenerator;
+        $generator = new RandomEnglishGenerator();
         $generator->setConfig('The [adjective] [noun] [verb] [preposition] the [noun]');
         $this->assertEquals('The quiet bank cover near the left.', $generator->sentence());
     }
 
     public function testComma(): void
     {
-        $generator = new RandomEnglishGenerator;
+        $generator = new RandomEnglishGenerator();
         $generator->setConfig('It was [adjective] in the [noun], [contraction] [noun] was [adjective]');
         $this->assertEquals('It was quiet in the bank, your bread was low.', $generator->sentence());
     }
 
     public function testTitle(): void
     {
-        $generator = new RandomEnglishGenerator;
+        $generator = new RandomEnglishGenerator();
         $generator->setConfig('It was [adjective] in the [noun], [contraction] [noun] was [adjective]');
         $this->assertEquals('It Was Quiet In The Bank, Your Bread Was Low', $generator->title());
     }
 
     public function testCapitalFirstWord(): void
     {
-        $generator = new RandomEnglishGenerator;
+        $generator = new RandomEnglishGenerator();
         $generator->setConfig('[control_verb]!!  You cannot [verb] here');
         $this->assertEquals('Order!! You cannot boat here.', $generator->sentence());
     }
 
     public function testParagraph(): void
     {
-        $generator = new RandomEnglishGenerator;
+        $generator = new RandomEnglishGenerator();
         \error_log($generator->paragraph());
     }
 
     public function skiptestLots(): void
     {
-        \srand(1000);
-        $generator = new RandomEnglishGenerator;
+        $generator = new RandomEnglishGenerator();
 
         for ($i=0; $i< 100; $i++) {
             \error_log($generator->sentence());
